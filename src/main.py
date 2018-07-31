ebebec5e5d0df54a314f6e311c03dbbff0eeb358
Nikita Alkhovik <forgoty13@gmail.com>
2018-08-01 01:39:06 +0300

    Fixing bug

--- src/main.py
+++ src/main.py
@@ -8,11 +8,11 @@ def main():
     namespace = parser.parse_args(sys.argv[1:])
     if namespace.command == 'download':
         if namespace.album_id == None:
-            service = DownloadService(namespace.user, namespace.path)
+            service = DownloadService(namespace.owner, namespace.path)
             for item in service.albums:
                 service.download_album(item.get('id'))
         else:
-            service = DownloadService(namespace.user, namespace.path)
+            service = DownloadService(namespace.owner, namespace.path)
             service.download_album(namespace.album_id)
     if not namespace.command:
         parser.print_help()
