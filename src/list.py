12eae4447e4203509af37b031a70d5d8a55ea238
Alkhovik Nikita <alkhovik.nikita@rambler.ru>
2019-04-10 14:01:11 +0300

    quick fix of list command

--- src/list.py
+++ src/list.py
@@ -3,5 +3,5 @@ from .download import DownloadSession
 
 def get_list(api, user, system=None):
     session = DownloadSession(api, user, system=system)
-    for album in session.get_all_albums:
+    for album in session.get_all_albums():
         print('id:{} - {}({})'.format(album.id, album.title, album.size))
